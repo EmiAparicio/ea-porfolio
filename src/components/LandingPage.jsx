1d31ebacd1d6d4042bd874661226903fafb887ae
NoahReaver <emilianojaparicio@gmail.com>
2022-09-23 20:29:08 -0300

    new button

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,25 +1,24 @@
 import "./LandingPage.css";
 import { useEffect } from "react";
-import bgImg from "./hexBG.png";
 
 export default function LandingPage() {
   useEffect(() => {
-    // const link = document.querySelectorAll('nav > .hover-this');
+    const buttons = document.querySelectorAll("span.btn");
     const cursor = document.getElementById("cursor");
+    const pointer = document.getElementById("pointer");
 
-    //       const animateit = function (e) {
-    //             const span = this.querySelector('span');
-    //             const { offsetX: x, offsetY: y } = e,
-    //             { offsetWidth: width, offsetHeight: height } = this,
+    const animateit = function (e) {
+      pointer.style.backgroundColor = `#383838`;
+      pointer.style.filter = `drop-shadow(0 0 3px #f0f0f0)`;
 
-    //             move = 25,
-    //             xMove = x / width * (move * 2) - move,
-    //             yMove = y / height * (move * 2) - move;
+      if (e.type === "mouseout") {
+        pointer.style.backgroundColor = `#f0f0f0`;
+        pointer.style.filter = ``;
+      }
+    };
 
-    //             span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-    //             if (e.type === 'mouseleave') span.style.transform = '';
-    //       };
+    buttons.forEach((b) => b.addEventListener("mousemove", animateit));
+    buttons.forEach((b) => b.addEventListener("mouseout", animateit));
 
     const editCursor = (e) => {
       const { clientX: x, clientY: y } = e;
@@ -27,31 +26,44 @@ export default function LandingPage() {
       cursor.style.left = x + "px";
       cursor.style.top = y + "px";
       cursor.style.transform = "translate(-50%, -50%) scale(1)";
+      pointer.style.left = x + "px";
+      pointer.style.top = y + "px";
+      pointer.style.transform = "translate(-50%, -50%) scale(1)";
     };
 
     const editCursorOut = (e) => {
       const { clientX: x, clientY: y } = e;
-      if (x <= 0 || y <= 0 || x >= window.innerWidth || y >= window.innerHeight)
+      if (
+        x <= 0 ||
+        y <= 0 ||
+        x >= window.innerWidth ||
+        y >= window.innerHeight
+      ) {
         cursor.style.transform = "translate(-50%, -50%) scale(0)";
+        pointer.style.transform = "translate(-50%, -50%) scale(0)";
+      }
     };
 
-    // link.forEach(b => b.addEventListener('mousemove', animateit));
-    // link.forEach(b => b.addEventListener('mouseleave', animateit));
     window.addEventListener("mousemove", editCursor);
     window.addEventListener("mouseout", editCursorOut);
   }, []);
 
   return (
-    <div className="bg">
-      <div className="bgImg">
-        <div id="cursor" className="cursor" />
-        <div className="lighter" />
-        <div className="lighter2" />
-        <div className="lighter3" />
-        {/* <div className="titleLighter" /> */}
+    <>
+      <div className="bg">
+        <div className="bgImg">
+          <div id="cursor" className="cursor" />
+          <div id="pointer" className="pointer" />
+
+          <div className="lighter" />
+          <div className="lighter2" />
+          <div className="lighter3" />
+
+          <span className="btn">Call</span>
+        </div>
+        <h1 className="title">EMILIANO APARICIO</h1>
+        <h2 className="subtitle">Full Stack Web Developer</h2>
       </div>
-      <h1 className="title">EMILIANO APARICIO</h1>
-      <h2 className="subtitle">Full Stack Web Developer</h2>
-    </div>
+    </>
   );
 }
