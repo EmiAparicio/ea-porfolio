a7b6b4f8e0ceef3dc74e26eb6a307c03c7fc9a1d
NoahReaver <emilianojaparicio@gmail.com>
2025-08-05 16:29:41 -0300

    pre-refactor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-
-import { App } from "./App";
-// import { store } from "./redux/store";
-
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <Provider store={store}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    {/* </Provider> */}
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
