b4c1fd5980c54fc420de14c0b381a03fd42fb59d
NoahReaver <emilianojaparicio@gmail.com>
2025-10-11 12:50:45 -0300

    feat: add PageModal component

--- src/app/projects/portfolio/atoms/modalAtoms.ts
+++ src/app/projects/portfolio/atoms/modalAtoms.ts
@@ -1,5 +1,6 @@
 'use client';
 
+import { HexToggleItem } from '@portfolio/components/HexToggleBar/HexToggleBar';
 import { atom } from '@portfolio/lib/jotai';
 import type { ReactNode } from 'react';
 
@@ -7,3 +8,5 @@ export type ModalContent = { child: ReactNode; info?: string };
 
 export const globalModalOpenAtom = atom<boolean>(false);
 export const globalModalContentAtom = atom<ModalContent | null>(null);
+
+export const globalModalItemsAtom = atom<HexToggleItem[]>([]);
